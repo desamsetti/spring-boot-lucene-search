96487ce2d58504838024f9fa7ee02a327445d8d7
Harshith Desamsetti <dharshith1996@gmail.com>
2020-10-28 17:52:19 -0500

    Initial Commit

---
+++ src/main/java/com/hdesamsetti/springbootlucenesearch/SpringBootLuceneSearchApplication.java
@@ -0,0 +1,13 @@
+package com.hdesamsetti.springbootlucenesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLuceneSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLuceneSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hdesamsetti/springbootlucenesearch/SpringBootLuceneSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdesamsetti.springbootlucenesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLuceneSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
